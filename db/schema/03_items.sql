6fff0672e8a67e7ad0ffd1f1c970a29b5e3b3da1
tsumitakaoru <k.tsumita01@gmail.com>
2021-11-28 20:18:16 -0500

    Fix FK

--- db/schema/03_items.sql
+++ db/schema/03_items.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS items CASCADE;
 CREATE TABLE items (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
-  descrition TEXT NOT NULL,
+  descrition TEXT,
   price INTEGER  NOT NULL DEFAULT 0,
-  thumbnail_url VARCHAR(255) NOT NULL,
+  thumbnail_url VARCHAR(255),
   category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE
 );
