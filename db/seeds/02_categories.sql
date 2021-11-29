c5a437c37c05e05f5ee0469b2cb2c39c81985450
tsumitakaoru <k.tsumita01@gmail.com>
2021-11-28 20:20:28 -0500

    Add seeds

---
+++ db/seeds/02_categories.sql
@@ -0,0 +1,7 @@
+INSERT INTO categories (name)VALUES ('Appetizers');
+INSERT INTO categories (name)VALUES ('Salads');
+INSERT INTO categories (name)VALUES ('Pasta');
+INSERT INTO categories (name)VALUES ('Desserts');
+INSERT INTO categories (name)VALUES ('Beverages');
+
+
---
+++ db/seeds/05_order_items.sql
@@ -0,0 +1,45 @@
+--order_id:1
+INSERT INTO order_items (order_id, item_id, quantity)
+VALUES (1, 1, 1);
+INSERT INTO order_items (order_id, item_id, quantity)
+VALUES (1, 3, 2);
+INSERT INTO order_items (order_id, item_id, quantity)
+VALUES (1, 7, 2);
+
+--order_id:2
+INSERT INTO order_items (order_id, item_id, quantity)
+VALUES (2, 2, 1);
+INSERT INTO order_items (order_id, item_id, quantity)
+VALUES (2, 13, 2);
+INSERT INTO order_items (order_id, item_id, quantity)
+VALUES (2, 11, 1);
+INSERT INTO order_items (order_id, item_id, quantity)
+VALUES (2, 8, 3);
+INSERT INTO order_items (order_id, item_id, quantity)
+VALUES (2, 9, 1);
+INSERT INTO order_items (order_id, item_id, quantity)
+VALUES (2, 17, 1);
+INSERT INTO order_items (order_id, item_id, quantity)
+VALUES (2, 20, 1);
+
+--order_id:3
+INSERT INTO order_items (order_id, item_id, quantity)
+VALUES (3, 1, 6);
+INSERT INTO order_items (order_id, item_id, quantity)
+VALUES (3, 7, 3);
+INSERT INTO order_items (order_id, item_id, quantity)
+VALUES (3, 15, 2);
+INSERT INTO order_items (order_id, item_id, quantity)
+VALUES (3, 16, 4);
+
+--order_id:4
+INSERT INTO order_items (order_id, item_id, quantity)
+VALUES (4, 4, 1);
+INSERT INTO order_items (order_id, item_id, quantity)
+VALUES (4, 5, 7);
+INSERT INTO order_items (order_id, item_id, quantity)
+VALUES (4, 6, 1);
+INSERT INTO order_items (order_id, item_id, quantity)
+VALUES (4, 12, 1);
+INSERT INTO order_items (order_id, item_id, quantity)
+VALUES (4, 14, 1);
