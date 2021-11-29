8c5d70b6ef475a03fa8ce7dc118f3bc00cfcb320
tsumitakaoru <k.tsumita01@gmail.com>
2021-11-29 17:10:23 -0500

    Change 01_customers.sql and 04_orders.sql

--- db/schema/04_orders.sql
+++ db/schema/04_orders.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS orders CASCADE;
 CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
   customer_id INTEGER REFERENCES customers(id) ON DELETE CASCADE,
+  status VARCHAR(255) DEFAULT 'Placed Order',
   place_order_time TIMESTAMP,
   estimated_time TIMESTAMP,
   completed_time TIMESTAMP
