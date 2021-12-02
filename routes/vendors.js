90703179efa42bf2d395df441c2627c7c0abd66f
tsumitakaoru <k.tsumita01@gmail.com>
2021-12-02 14:38:52 -0500

    Add order details query for sms

--- routes/vendors.js
+++ routes/vendors.js
@@ -4,6 +4,12 @@ const router = express.Router();
 const bodyParser = require("body-parser");
 router.use(bodyParser.urlencoded({ extended: true }));
 
+const accountSid = 'AC21417ecef39f48e33fbf3deb537ab629';
+const authToken = '37f530bb6d8119d61f8794e789216c26';
+const client = require('twilio')(accountSid, authToken);
+
+
+
 module.exports = (db) => {
 
 
@@ -64,16 +70,41 @@ module.exports = (db) => {
       const queryParams = [];
       queryParams.push(estimatedTime);
       queryParams.push(orderId);
-      console.log(queryParams);
       const updateQueryString = `
-    UPDATE orders SET estimated_time = NOW() + $1 * interval '1 minutes', status = 'In progress'
-    WHERE id = $2 RETURNING *;
-    `;
-      db.query(updateQueryString, queryParams)
-        .then(data => {
-          // const order = data.rows[0];
-          // res.json({ order });
-          res.redirect(`/vendors/1/order/${orderId}`);
+      UPDATE orders SET estimated_time = NOW() + $1 * interval '1 minutes', status = 'In progress'
+      WHERE id = $2 RETURNING *;
+      `;
+      const orderDetailsQueryString = `
+      SELECT order_id, customers.first_name, customers.last_name, items.name, items.price, quantity
+      FROM order_items
+      JOIN orders ON orders.id = order_id
+      JOIN customers ON customers.id = customer_id
+      JOIN items ON items.id = item_id
+      WHERE order_id = $1;
+      `;
+
+      const orderPromise = db.query(updateQueryString, queryParams);
+      const orderDetailsPromise = db.query(orderDetailsQueryString, [orderId]);
+      Promise.all([orderPromise, orderDetailsPromise])
+        .then(response => {
+          const order = response[0].rows;
+          const orderDetails = response[1].rows;
+          const estimatedTime = order.estimated_time;
+          const firstName = orderDetails[0].first_name;
+          console.log(order.estimated_time);
+          const msg = `Hello ${firstName}. your order No. ${orderId} is confirmed, please pick it up at ${estimatedTime}`;
+          console.log(msg);
+          res.json({ order });
+          // if (order) {
+          //   client.messages
+          //     .create({
+          //       body: 'Your order is accepted. Estimated time: 18:00 EST',
+          //       from: '+12264000625',
+          //       to: '+16474256464'
+          //     })
+          //     .then(message => console.log(message.sid));
+          // }
+          // res.redirect(`/vendors/1/order/${orderId}`);
         })
         .catch(err => {
           console.log(err);
@@ -104,9 +135,7 @@ module.exports = (db) => {
     }
 
     // const msg = “hello (name) your order #12343 is confirmed, please pick it up at 1:30
-    const accountSid = 'AC21417ecef39f48e33fbf3deb537ab629';
-    const authToken = '37f530bb6d8119d61f8794e789216c26';
-    const client = require('twilio')(accountSid, authToken);
+
 
     // if (estimatedTime) {
     //   client.messages
