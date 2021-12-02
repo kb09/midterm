1532ac6c9407a60e9a815e2a39b97bc4f332fcb1
tsumitakaoru <k.tsumita01@gmail.com>
2021-12-02 09:56:10 -0500

    Add twilio test code and change object name

--- routes/vendors.js
+++ routes/vendors.js
@@ -16,7 +16,7 @@ module.exports = (db) => {
       WHERE order_id = $1;
       `;
     const totalQueryString = `
-      SELECT orders.id as order_id, SUM(order_items.quantity) AS total_item, SUM(items.price * order_items.quantity) AS subtotal
+      SELECT orders.id as order_id, SUM(order_items.quantity) AS total_item
       FROM orders
       JOIN order_items ON order_items.order_id = orders.id
       JOIN items ON items.id = order_items.item_id
@@ -35,13 +35,13 @@ module.exports = (db) => {
     const statusPromise = db.query(statusString, [orderId]);
     Promise.all([orderPromise, totalPromise, statusPromise])
       .then(response => {
-        const order = response[0].rows;
+        const orders = response[0].rows;
         const total = response[1].rows;
         const status = response[2].rows;
 
-        const templateVars = { order, total, status };
+        const templateVars = { orders, total, status };
         // res.json(templateVars);
-        res.render("checkout", templateVars); //ejs file need to be changed
+        res.render("vendors", templateVars); //ejs file need to be changed
 
       })
       .catch(err => {
@@ -53,8 +53,18 @@ module.exports = (db) => {
 
   });
   router.post("/:id/:orders/:order_id", (req, res) => {
-    res.json(`I'm a id ${req.params.id}, orders: ${req.params.orders}, order_id${req.params.order_id}`);
+    // res.json(`I'm a id ${req.params.id}, orders: ${req.params.orders}, order_id${req.params.order_id}`);
+    const accountSid = 'AC21417ecef39f48e33fbf3deb537ab629';
+    const authToken = '37f530bb6d8119d61f8794e789216c26';
+    const client = require('twilio')(accountSid, authToken);
 
+    client.messages
+      .create({
+        body: 'Your order is accepted. Estimated time: 18:00 EST',
+        from: '+12264000625',
+        to: '+16474256464'
+      })
+      .then(message => console.log(message.sid));
   });
 
   return router;
