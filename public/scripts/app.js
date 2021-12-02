2e98ee5b4382f0034171d056c8bbff65f44f3ffc
kartik <kabakshi@hotmail.ca>
2021-12-01 20:13:36 -0500

    passing data from seeds and updating layout

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,69 @@
 // Client facing scripts here
+const producetsE1= document.querySelector("/../db/schema/03_items.sql")
+
+//  add item to cart
+
+let cartItem = {}
+
+function addToCart(){
+  let addItem = document.getElementById("add");
+  let delItem = document.getElementById("delete");
+  if(addItem.clicked == true){
+    cartItem.push(items[item].id)
+  }
+}
+
+
+// Adding items to cart
+
+// if button is cliecked --> ( event listner)
+// add item name,price, id to cart
+
+
+
+
+
+
+
+
+
+// document.getElementById("add").onclick = function() {
+//   addToCart(${items.id})
+// }
+
+// // cart array
+// let cart = [];
+// // function addToCart
+
+// function addToCart(id){
+//   // check if product already exist in cart
+//   if(cart.some((product) => product.id === id)){
+//     alert("product already in cart")
+//   } else{
+//     const product = items.find((item) => item.id === id)
+//     cart.push({
+//       ...product,
+//       numberOfUnits : 1
+//     })
+
+
+
+//   }
+
+// updateCart();
+
+// }
+
+// //update cart function
+// function updateCart(){
+//   renderCartItems();
+//   // renderSubtotal();
+// }
+
+// // render cart items
+
+
+// // when button is clicked add to cart
+
+
+
--- routes/cart.js
+++ routes/cart.js
@@ -19,3 +19,57 @@ module.exports = (db) => {
 
   return router;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// adding item to cart
+
+// module.exports = function Cart(oldCart){
+//   this.items= oldCart.items || {};
+//   this.totalQty = oldCart.totalQty || 0;
+//   this.totalPrice = oldCart.totalPrice || 0;
+
+//   // add to cart when adding more items
+
+//   this.add = function(item, id){
+//     let storedItem = this.items[id];
+//     if (!storedItem){
+//       storedItem = this.items[id] = {item: item, qty:0, price:0};
+//     }
+//     storedItem.qty++;
+//     storedItem.price = storedItem.item.price * storedItem.qty;
+//     this.totalQty++;
+//     this.totalPrice += storedItem.item.price;
+
+//   }
+
+//       this.generateArray=function(){
+//         let arr =[];
+//         for (let id in this.items){
+//           arr.push(this.items[id]);
+//         }
+//         return arr;
+//       }
+
+//       return router;
+
+// };
